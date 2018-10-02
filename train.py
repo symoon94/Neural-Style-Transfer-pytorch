e4726c46d03f53e5f323c1141c1030282346e397
symoon94 <msy0128@gmail.com>
2018-10-02 10:02:40 +0900

    rename img

--- train.py
+++ train.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--content_img', type=str, default = 'images/dancing.jpg')
     parser.add_argument('--style_img', type=str, default = 'images/picasso.jpg')
-    parser.add_argument('--size', type=int, default = 128)
+    parser.add_argument('--size', type=int, default = 128, help='if you want to get more clear pictures, increase the size')
     parser.add_argument('--steps', type=int, default = 300)
     parser.add_argument('--c_weight', type=int, default = 1, help='weighting factor for content reconstruction')
     parser.add_argument('--s_weight', type=int, default = 100000, help='weighting factor for style reconstruction')
